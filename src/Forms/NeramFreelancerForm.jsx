1cbfb8bbee8d6c97b12861d43dc06d62efd2c8f7
fayasfanu <fayasfanu00@gmail.com>
2025-02-15 12:24:32 +0530

    added

--- src/Forms/NeramFreelancerForm.jsx
+++ src/Forms/NeramFreelancerForm.jsx
@@ -43,7 +43,7 @@ const NeramFreelancerForm = () => {
      <div className="min-h-screen bg-gray-50 flex items-center main-bg justify-center px-4">
        <div className="max-w-md w-full space-y-8">
          <div className="text-center">
-           <h2 className="mt-6 text-3xl font-bold text-white-900">
+           <h2 className="mt-6 text-3xl font-bold text-white">
              Neram Freelancer
            </h2>
            <p className="mt-2 text-sm text-violet-50">
